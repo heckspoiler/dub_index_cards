88e3865393bd13ef330a412c35cb172973ccb604
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-02-10 22:06:01 +0100

    added some js, some visual effects

--- js-files/landing_page.js
+++ js-files/landing_page.js
@@ -1,5 +1,12 @@
 const btns = document.querySelectorAll(".landing__page--button");
 const title = document.querySelector(".landing__page--title");
+const text = document.querySelector(".landing__page--intro--text--p");
+
+window.onload = function () {
+  setTimeout(function () {
+    title.style.animation = "slide-in 1s ease-in-out forwards";
+  }, 500);
+};
 
 document.addEventListener("DOMContentLoaded", function () {
   const elements = document.querySelectorAll(
@@ -14,4 +21,13 @@ document.addEventListener("DOMContentLoaded", function () {
       element.style.opacity = 1;
     }, 500);
   });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  text.style.opacity = 0;
+
+  setTimeout(() => {
+    text.style.transition = "opacity 1s ease-out";
+    text.style.opacity = 1;
+  }, 200);
 });
