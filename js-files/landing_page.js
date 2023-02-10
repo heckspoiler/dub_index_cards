64be20a7008cda448f9e34b58a46dcc27597adb2
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-02-10 22:47:49 +0100

    added js animations for icons

--- js-files/landing_page.js
+++ js-files/landing_page.js
@@ -1,11 +1,26 @@
 const btns = document.querySelectorAll(".landing__page--button");
 const title = document.querySelector(".landing__page--title");
 const text = document.querySelector(".landing__page--intro--text--p");
+const alien = document.querySelector(".fa-alien-8bit");
+const brackets = document.querySelector(".fa-brackets-curly");
 
 window.onload = function () {
   setTimeout(function () {
     title.style.animation = "slide-in 1s ease-in-out forwards";
   }, 500);
+  alien.style.left = "-200px";
+  alien.style.transform = "rotate(-140deg)";
+  setTimeout(function () {
+    alien.style.transition = "left 1s ease-out, transform 1s ease-out";
+    alien.style.left = "5vh";
+    alien.style.transform = "rotate(-40deg)";
+  }, 400);
+  brackets.style.left = "100vw";
+  brackets.style.transform = "rotate(160deg)";
+  setTimeout(function () {
+    brackets.style.transition = "left 1s ease-out, transform 1s ease-in";
+    brackets.style.left = "70vw";
+  }, 800);
 };
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -27,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function () {
   text.style.opacity = 0;
 
   setTimeout(() => {
-    text.style.transition = "opacity 1s ease-out";
+    text.style.transition = "opacity 1.7s ease-in-out";
     text.style.opacity = 1;
-  }, 200);
+  }, 600);
 });
