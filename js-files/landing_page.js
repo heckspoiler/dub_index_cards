6908afae1e3e8e7246a4d9040f5fd185762fdae9
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-02-10 21:51:58 +0100

    css changes, first js added

--- js-files/landing_page.js
+++ js-files/landing_page.js
@@ -1,1 +1,17 @@
+const btns = document.querySelectorAll(".landing__page--button");
+const title = document.querySelector(".landing__page--title");
 
+document.addEventListener("DOMContentLoaded", function () {
+  const elements = document.querySelectorAll(
+    ".landing__page--background--icon"
+  );
+
+  elements.forEach((element) => {
+    element.style.opacity = 0;
+
+    setTimeout(() => {
+      element.style.transition = "opacity 1s ease-out";
+      element.style.opacity = 1;
+    }, 500);
+  });
+});
