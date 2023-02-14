85e8ed9c68037fe4113d79e52e50d36714273e2a
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-02-14 15:46:17 +0100

    changes to everything

--- about files/about.js
+++ about files/about.js
@@ -2,15 +2,10 @@ const paragraphs = document.querySelectorAll(
   ".about__section--paragraphs--paragraph"
 );
 
-document
-  .querySelector(".about__paragraphs")
-  .addEventListener("click", function (event) {
-    if (
-      event.target.className === "arrow" ||
-      event.target.className === "button"
-    ) {
-      let targetElement = event.target.paragraphs;
-      targetElement.style.display =
-        targetElement.style.display === "none" ? "block" : "none";
-    }
+paragraphs.forEach(function (paragraph) {
+  const button = paragraph.previousElementSibling;
+  button.addEventListener("click", function () {
+    paragraph.style.display =
+      paragraph.style.display === "none" ? "block" : "none";
   });
+});
