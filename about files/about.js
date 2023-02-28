b8e16d3ffad6ec010e7e065c6351a3c21cbaca89
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-03-01 00:41:41 +0100

    css media queries about.html

--- about files/about.js
+++ about files/about.js
@@ -6,14 +6,14 @@ const paragraphs = document.querySelectorAll(
   ".about__section--paragraphs--paragraph"
 );
 
-paragraphs.forEach(function (paragraph) {
-  const button = paragraph.previousElementSibling;
-  button.addEventListener("click", function () {
-    paragraph.style.display =
-      paragraph.style.display === "none" ? "block" : "none";
-    paragraph.style.transition === "display 2s ease-in-out";
-  });
-});
+// paragraphs.forEach(function (paragraph) {
+//   const button = paragraph.previousElementSibling;
+//   button.addEventListener("click", function () {
+//     paragraph.style.display =
+//       paragraph.style.display === "none" ? "block" : "none";
+//     paragraph.style.transition === "display 2s ease-in-out";
+//   });
+// });
 
 backArrow.addEventListener("click", function () {
   window.location = "../landing_page/landing_page.html";
