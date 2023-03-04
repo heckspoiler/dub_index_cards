23aeeb2c4806b4c59d16ca457b0673858920206a
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-03-04 18:48:22 +0100

    added some loading effects and js

--- card_editor/js_files/card_editor_main_styling.js
+++ card_editor/js_files/card_editor_main_styling.js
@@ -2,6 +2,11 @@
 
 const plusSign = document.querySelector(".add-folder-p");
 const folderImage = document.querySelector(".folder_image");
+const body = document.querySelector("body");
+const door = document.querySelector(".logout__door");
+const missingFolderTitle = document.querySelector(".folder_section--title");
+const missingFolderSmiley = document.querySelector(".folder_section--smiley");
+const pointingArrow = document.querySelector(".pointing_arrow");
 
 console.log(plusSign);
 
@@ -11,4 +16,18 @@ plusSign.addEventListener("mouseover", () => {
 
 plusSign.addEventListener("mouseout", () => {
   folderImage.style.opacity = "0.6";
+});
+
+window.addEventListener("load", (event) => {
+  body.style.opacity = "1";
+  door.style.marginTop = "4rem";
+  setTimeout(function () {
+    missingFolderTitle.style.opacity = 1;
+  }, 1000);
+  setTimeout(function () {
+    missingFolderSmiley.style.opacity = 1;
+  }, 2500);
+  setTimeout(function () {
+    pointingArrow.style.opacity = 1;
+  }, 2500);
 });
