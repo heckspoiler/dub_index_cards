590e788691d1d27911d38c4245758bc723e9448f
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-03-04 19:48:08 +0100

    added stuff, now adding create form

--- card_editor/js_files/card_editor_main_styling.js
+++ card_editor/js_files/card_editor_main_styling.js
@@ -1,5 +1,3 @@
-//folder and cross on foldler
-
 const plusSign = document.querySelector(".add-folder-p");
 const folderImage = document.querySelector(".folder_image");
 const body = document.querySelector("body");
@@ -7,8 +5,6 @@ const door = document.querySelector(".logout__door");
 const missingFolderTitle = document.querySelector(".folder_section--title");
 const missingFolderSmiley = document.querySelector(".folder_section--smiley");
 const pointingArrow = document.querySelector(".pointing_arrow");
-
-console.log(plusSign);
 
 plusSign.addEventListener("mouseover", () => {
   folderImage.style.opacity = "1";
@@ -18,16 +14,12 @@ plusSign.addEventListener("mouseout", () => {
   folderImage.style.opacity = "0.6";
 });
 
-window.addEventListener("load", (event) => {
+document.addEventListener("DOMContentLoaded", () => {
   body.style.opacity = "1";
   door.style.marginTop = "4rem";
-  setTimeout(function () {
+  setTimeout(() => {
     missingFolderTitle.style.opacity = 1;
-  }, 1000);
-  setTimeout(function () {
     missingFolderSmiley.style.opacity = 1;
-  }, 2500);
-  setTimeout(function () {
     pointingArrow.style.opacity = 1;
   }, 2500);
 });
