554c0213697832ee14dad50459d88b69ec3d4256
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-03-06 13:17:31 +0100

    changes to html, css and js. added functionality

--- card_editor/js_files/folder_functionality.js
+++ card_editor/js_files/folder_functionality.js
@@ -1,5 +1,5 @@
 addButton.addEventListener("click", (event) => {
-  event.preventDefault(); // prevents the default behavior of the button
+  event.preventDefault();
 
   const folderDiv = document.createElement("div");
   folderDiv.className = "added_folder--div";
@@ -9,21 +9,26 @@ addButton.addEventListener("click", (event) => {
   const tagsInput = document.querySelector("#folder-tags");
 
   folderDiv.innerHTML = `
-      <img src="../icons_and_images/folderPix.png" class="added_folder--img" />
-      <div class="added_folder--paragraph-container">
-        <p class="added_folder--folder-name">${folderNameInput.value}</p>
-        <p class="added_folder--topic">${topicInput.value}</p>
-        <p class="added_folder--chapter">${chapterInput.value}</p>
-        <p class="added_folder--tags">${tagsInput.value}</p>
-      </div>
-    `;
+        <img src="../icons_and_images/folderPix.png" class="added_folder--img" />
+        <div class="added_folder--paragraph-container">
+          <p class="added_folder--folder-name">${folderNameInput.value.toUpperCase()}</p>
+          <p class="added_folder--topic">${topicInput.value}</p>
+          <p class="added_folder--chapter">${chapterInput.value}</p>
+          <p class="added_folder--tags">${tagsInput.value.toLowerCase()}</p>
+        </div>
+      `;
 
   folderNameInput.value = "";
   topicInput.value = "";
   chapterInput.value = "";
   tagsInput.value = "";
 
-  folderSection.appendChild(folderDiv);
+  if (folderSection.children.length === 0) {
+    folderSection.appendChild(folderDiv);
+  } else {
+    folderSection.insertBefore(folderDiv, folderSection.firstElementChild);
+  }
+
   formSection.style.visibility = "hidden";
   plusSign.style.visibility = "visible";
   mainSection.style.opacity = 1;
