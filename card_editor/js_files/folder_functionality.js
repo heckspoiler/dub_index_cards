c22fe7fb297dc8c89e24eca60565bfa170502762
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-03-06 11:38:46 +0100

    customizing folders

--- card_editor/js_files/folder_functionality.js
+++ card_editor/js_files/folder_functionality.js
@@ -3,19 +3,30 @@ addButton.addEventListener("click", (event) => {
 
   const folderDiv = document.createElement("div");
   folderDiv.className = "added_folder--div";
+  const folderNameInput = document.querySelector("#folder-name");
+  const topicInput = document.querySelector("#folder-topic");
+  const chapterInput = document.querySelector("#folder-chapter");
+  const tagsInput = document.querySelector("#folder-tags");
+
   folderDiv.innerHTML = `
       <img src="../icons_and_images/folderPix.png" class="added_folder--img" />
       <div class="added_folder--paragraph-container">
-        <p class="added_folder--folder-name">Folder Name</p>
-        <p class="added_folder--topic">Topic</p>
-        <p class="added_folder--chapter">Chapter</p>
-        <p class="added_folder--tags">Tags</p>
+        <p class="added_folder--folder-name">${folderNameInput.value}</p>
+        <p class="added_folder--topic">${topicInput.value}</p>
+        <p class="added_folder--chapter">${chapterInput.value}</p>
+        <p class="added_folder--tags">${tagsInput.value}</p>
       </div>
     `;
+
+  folderNameInput.value = "";
+  topicInput.value = "";
+  chapterInput.value = "";
+  tagsInput.value = "";
 
   folderSection.appendChild(folderDiv);
   formSection.style.visibility = "hidden";
   plusSign.style.visibility = "visible";
   mainSection.style.opacity = 1;
   mainWordingSection.style.visibility = "hidden";
+  inputFields.value = "";
 });
