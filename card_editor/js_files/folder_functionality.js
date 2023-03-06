a400d88add16cda5539c4a4d3f8abe9c3854cf92
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-03-06 01:38:36 +0100

    adding functionality

--- card_editor/js_files/folder_functionality.js
+++ card_editor/js_files/folder_functionality.js
@@ -1,2 +1,20 @@
 const folderSection = document.querySelector(".folder_section--added-folders");
 const addButton = document.querySelector("#add-folder-button");
+const folderDiv = document.createElement("div");
+folderDiv.className = "added_folder--div";
+folderDiv.innerHTML = `
+<img
+  src="../icons_and_images/folderPix.png"
+  class="added_folderfolder--img"
+/>
+<div class="added_folder--paragraph-container">
+  <p class="added_folder--folder-name">Folder Name</p>
+  <p class="added_folder--topic">Topic</p>
+  <p class="added_folder--chapter">Chapter</p>
+  <p class="added_folder--tags">Tags</p>
+</div>
+`;
+
+addButton.addEventListener("click", () => {
+  folderSection.appendChild(folderDiv);
+});
